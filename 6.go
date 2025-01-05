63932f29624b639e94ad09c6ccd496e992bc0380
Tomáš Bašta <tbasta97@gmail.com>
2025-01-05 15:58:46 +0100

    fixed day6

--- 6.go
+++ 6.go
@@ -1,48 +1,62 @@
 package main
 
-func getMapStart(m [][]byte) (x, y int) {
-	for i := range len(m) {
-		for j := range m[i] {
-			if m[i][j] == '^' {
-				m[i][j] = '.'
-				return i, j
-			}
-		}
-	}
-	return -1, -1
-}
-
-func solve6a() {
-	var m [][]byte
-	var s string
-	for scan(&s) {
-		m = append(m, []byte(s))
-	}
-
-	var res int64
-	x, y := getMapStart(m)
-
-	dirs := [][2]int{{-1, 0}, {0, 1}, {1, 0}, {0, -1}}
-	dirIdx := 0
-	for {
-		if m[x][y] == '.' {
-			res++
-			m[x][y] = 'X'
+func simulateGuard(guardMap [][]byte, x, y, dir int) (int, bool) {
+	visited := make(map[[2]int]struct{})
+	visitedWDir := make(map[[3]int]struct{})
+	for get2dVal(guardMap, x, y) != 0 {
+		if _, ok := visitedWDir[[3]int{x, y, dir}]; ok {
+			return 0, true
 		}
 
-		nextX, nextY := x+dirs[dirIdx][0], y+dirs[dirIdx][1]
-		if nextX < 0 || nextX >= len(m) ||
-			nextY < 0 || nextY >= len(m) {
-			break
-		}
+		visited[[2]int{x, y}] = struct{}{}
+		visitedWDir[[3]int{x, y, dir}] = struct{}{}
 
-		if m[nextX][nextY] == '#' {
-			dirIdx = (dirIdx + 1) % 4
+		nextX, nextY := move2d(x, y, dirs[dir])
+		if get2dVal(guardMap, nextX, nextY) == '#' {
+			dir = (dir + 1) % 4
 			continue
 		}
 
 		x, y = nextX, nextY
 	}
 
-	printToOut(res)
+	return len(visited), false
+}
+
+func solve6a() {
+	guardMap := read2dBytes()
+	x, y := findMarkIn2dSlice(guardMap, '^')
+
+	visited, _ := simulateGuard(guardMap, x, y, 0)
+	printToOut(visited)
+}
+
+func solve6b() {
+	guardMap := read2dBytes()
+	x, y := findMarkIn2dSlice(guardMap, '^')
+	dir := 0
+
+	visited := make(map[[2]int]struct{})
+	loopMakers := make(map[[2]int]struct{})
+	for get2dVal(guardMap, x, y) != 0 {
+		visited[[2]int{x, y}] = struct{}{}
+		nextX, nextY := move2d(x, y, dirs[dir])
+		if get2dVal(guardMap, nextX, nextY) == '#' {
+			dir = (dir + 1) % 4
+			continue
+		}
+
+		if _, passed := visited[[2]int{nextX, nextY}]; !passed && get2dVal(guardMap, nextX, nextY) == '.' {
+			guardMap[nextY][nextX] = '#'
+			_, loop := simulateGuard(guardMap, x, y, dir)
+			if loop {
+				loopMakers[[2]int{nextX, nextY}] = struct{}{}
+			}
+			guardMap[nextY][nextX] = '.'
+		}
+
+		x, y = nextX, nextY
+	}
+
+	printToOut(len(loopMakers))
 }
